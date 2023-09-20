92afa2797bec811cb77159a04863201a44b3fcd6
flowcore-platform <gitops@flowcore.io>
2023-09-20 16:31:53 +0100

    feat: initial commit

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 import os
+import json
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -38,8 +39,16 @@ Transformer
 
 @app.post("/transform")
 async def transform(body: Body):
-    print("received body", body)
-    return {"eventId": body.eventId, "validTime": body.validTime, **body.payload.model_dump()}
+    result = {}
+    for key, value in body.payload.dict().items():
+        # Check if the value is a string
+        if not isinstance(value, str):
+            # Convert non-strings to JSON format using json.dumps()
+            value = json.dumps(value, separators=(",",":"))
+        # Add the key-value pair to the result object
+        result[key] = value
+
+    return {"eventId": body.eventId, "validTime": body.validTime, **result}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ["PORT"]), log_level="info")
